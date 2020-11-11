3dc1d3e5300e8ce2345801f9cbfb7c027c01ebd5
N-ANGLO-5165\Lucas <lucas.tarta@hotmail.com>
2020-11-11 18:29:51 -0300

    ãlteracoes no painel de correcoes

--- public/views/secaomenu/gerenciar-secoes-menus.php
+++ public/views/secaomenu/gerenciar-secoes-menus.php
@@ -4,7 +4,7 @@
 
 	$retorno = null;
 
-	include_once ABSPATH . 'app/funcoesGlobais/paginacao.php';
+	include_once ABSPATH . 'backend/app/funcoesGlobais/paginacao.php';
 
 	if (!empty($this->dados->retorno))
 		$retorno = $this->dados->retorno;
@@ -25,13 +25,13 @@
 ?>
 
 <!-- Breadcrumb-->
-<ol class="breadcrumb">
+<!-- <ol class="breadcrumb">
     <li class="breadcrumb-item">
         <a href="<?= URL ?>">Dashboard</a>
     </li>
     <li class="breadcrumb-item active">Permissões</li>
     <li class="breadcrumb-item active">Seções de Menus</li>
-</ol>
+</ol> -->
 
 <div class="animated fadeIn">
 
